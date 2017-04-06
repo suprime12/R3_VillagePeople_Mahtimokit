923c8643b87b0f30cd9fd5aa8356c05b83c27a26
suprime12 <miika.piitulainen@hotmail.com>
2017-04-06 20:25:38 +0300

    virhepopupit (#39)
    * testi
    
    tehtiin jotain
    
    * testi 2
    
    asdfasdf
    
    * virhe popupit
    
    Virhe popuppeja lisätty

---
+++ R3_VillagePeople_Mahtimokit/AreYouSure_popup.designer.cs
@@ -0,0 +1,87 @@
+﻿namespace R3_VillagePeople_Mahtimokit
+{
+    partial class AreYouSure_popup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_AreYouSure = new System.Windows.Forms.Label();
+            this.btn_Yes = new System.Windows.Forms.Button();
+            this.btn_No = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_AreYouSure
+            // 
+            this.lbl_AreYouSure.AutoSize = true;
+            this.lbl_AreYouSure.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_AreYouSure.Location = new System.Drawing.Point(113, 32);
+            this.lbl_AreYouSure.Name = "lbl_AreYouSure";
+            this.lbl_AreYouSure.Size = new System.Drawing.Size(111, 20);
+            this.lbl_AreYouSure.TabIndex = 0;
+            this.lbl_AreYouSure.Text = "Oletko varma?";
+            // 
+            // btn_Yes
+            // 
+            this.btn_Yes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Yes.Location = new System.Drawing.Point(233, 69);
+            this.btn_Yes.Name = "btn_Yes";
+            this.btn_Yes.Size = new System.Drawing.Size(83, 39);
+            this.btn_Yes.TabIndex = 1;
+            this.btn_Yes.Text = "Ei";
+            this.btn_Yes.UseVisualStyleBackColor = true;
+            // 
+            // btn_No
+            // 
+            this.btn_No.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_No.Location = new System.Drawing.Point(30, 69);
+            this.btn_No.Name = "btn_No";
+            this.btn_No.Size = new System.Drawing.Size(82, 39);
+            this.btn_No.TabIndex = 2;
+            this.btn_No.Text = "Kyllä";
+            this.btn_No.UseVisualStyleBackColor = true;
+            // 
+            // AreYouSure_popup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 123);
+            this.Controls.Add(this.btn_No);
+            this.Controls.Add(this.btn_Yes);
+            this.Controls.Add(this.lbl_AreYouSure);
+            this.Name = "AreYouSure_popup";
+            this.Text = "Varmistus";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_AreYouSure;
+        private System.Windows.Forms.Button btn_Yes;
+        private System.Windows.Forms.Button btn_No;
+    }
+}
---
+++ R3_VillagePeople_Mahtimokit/TextError_popup.designer.cs
@@ -0,0 +1,75 @@
+﻿namespace R3_VillagePeople_Mahtimokit
+{
+    partial class TextError_popup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Error = new System.Windows.Forms.Button();
+            this.lbl_Errortext = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Error
+            // 
+            this.btn_Error.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Error.Location = new System.Drawing.Point(140, 82);
+            this.btn_Error.Name = "btn_Error";
+            this.btn_Error.Size = new System.Drawing.Size(75, 29);
+            this.btn_Error.TabIndex = 0;
+            this.btn_Error.Text = "OK";
+            this.btn_Error.UseVisualStyleBackColor = true;
+            // 
+            // lbl_Errortext
+            // 
+            this.lbl_Errortext.AutoSize = true;
+            this.lbl_Errortext.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Errortext.Location = new System.Drawing.Point(39, 24);
+            this.lbl_Errortext.Name = "lbl_Errortext";
+            this.lbl_Errortext.Size = new System.Drawing.Size(277, 40);
+            this.lbl_Errortext.TabIndex = 1;
+            this.lbl_Errortext.Text = "Virhe syöttäessä tietoja. \r\nTarkista, oletko syöttänyt tiedot oikein.";
+            this.lbl_Errortext.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // TextError_popup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 123);
+            this.Controls.Add(this.lbl_Errortext);
+            this.Controls.Add(this.btn_Error);
+            this.Name = "TextError_popup";
+            this.Text = "Virhe!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Error;
+        private System.Windows.Forms.Label lbl_Errortext;
+    }
+}
